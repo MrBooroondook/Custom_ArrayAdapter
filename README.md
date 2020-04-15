fb5bc9e1e561061139ca52bf168912f2ca705036
Aleksey Voko <voko.aleksey@gmail.com>
2020-04-16 00:45:53 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Custom ArrayAdapter
+Android Studio Training Project
---
+++ app/src/main/java/mr/booroondook/custom_arrayadapter/MainActivity.java
@@ -0,0 +1,14 @@
+package mr.booroondook.custom_arrayadapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
